082735b5230c1e561ff396089d71a54bd27e8e1e
Dhron Joshi <dhron.joshi@uwaterloo.ca>
2020-04-16 21:51:12 -0400

    make succinct var names

--- main.go
+++ main.go
@@ -16,13 +16,13 @@ func main() {
 		Usage: "swap [file1] [file2]",
 		Action: func(c *cli.Context) error {
 			if c.NArg() > 1 {
-				file1 := c.Args().Get(0)
-				file2 := c.Args().Get(1)
-				tmpName := "/tmp/" + file1 // create a temporary name
-				os.Rename(file1, tmpName)
-				os.Rename(file2, file1)
-				os.Rename(tmpName, file2)
-				fmt.Println("swapped " + file1 + " with " + file2)
+				fName1 := c.Args().Get(0)
+				fName2 := c.Args().Get(1)
+				tmpName := "/tmp/" + fName1 // create a temporary name
+				os.Rename(fName1, tmpName)
+				os.Rename(fName2, fName1)
+				os.Rename(tmpName, fName2)
+				fmt.Println("swapped " + fName1 + " with " + fName2)
 				return nil
 			}
 			return nil
